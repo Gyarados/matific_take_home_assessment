67b187e357bc73966d93fb27b54687188b750c1f
Gabriel Desiderá <5567681+Gyarados@users.noreply.github.com>
2025-03-09 21:21:39 -0300

    Change to Fargate service

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -28,43 +28,49 @@ export class EcrStack extends cdk.Stack {
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, ecrRepository: ecr.Repository, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      ipAddresses: ec2.IpAddresses.cidr("10.0.0.0/16"),
+      maxAzs: 2, // 1 public + 1 private subnets
+      vpcName: PREFIX + 'vpc',
+      restrictDefaultSecurityGroup: false,
     });
 
-    const asg = new autoscaling.AutoScalingGroup(this, PREFIX + 'asg', {
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
-      machineImage: ec2.MachineImage.latestAmazonLinux2023({
-        cpuType: ec2.AmazonLinuxCpuType.X86_64,
-      }),
-      minCapacity: 1,
-      maxCapacity: 3,
+    // const asg = new autoscaling.AutoScalingGroup(this, 'asg', {
+    //   autoScalingGroupName: PREFIX + 'asg',
+    //   instanceType: ec2.InstanceType.of(ec2.InstanceClass.T4G, ec2.InstanceSize.NANO),
+    //   machineImage: ec2.MachineImage.latestAmazonLinux2023({
+    //     cpuType: ec2.AmazonLinuxCpuType.X86_64,
+    //   }),
+    //   minCapacity: 1,
+    //   maxCapacity: 3,
+    //   vpc,
+    // });
+
+    const cluster = new ecs.Cluster(this, 'ecs-cluster', { 
       vpc,
+      clusterName: PREFIX + 'ecs-cluster',
     });
 
-    const cluster = new ecs.Cluster(this, PREFIX + 'ecs-cluster', { 
-      vpc,
-    });
-
-    const capacityProvider = new ecs.AsgCapacityProvider(this, PREFIX + 'asg-capacity-provider', {
-      autoScalingGroup: asg,
-      enableManagedTerminationProtection: true,
-      enableManagedScaling: true,
-    });
-
-    cluster.addAsgCapacityProvider(capacityProvider);
-  
-    const ecsService = new ecs_patterns.NetworkLoadBalancedEc2Service(this, "Ec2Service", {
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "Service", {
+      serviceName: PREFIX + 'service',
+      loadBalancerName: PREFIX + 'alb',
       cluster,
       memoryLimitMiB: 512,
+      cpu: 256, // 0.25 vCPU
       taskImageOptions: {
-        image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample"),
-      }
-    });
+        image: ecs.ContainerImage.fromEcrRepository(ecrRepository),
+        // environment: {
+        //   ENV_VAR_1: "value1",
+        //   ENV_VAR_2: "value2",
+        // },
+        containerPort: 8000
+      },
+      desiredCount: 1,
+    })
 
-    ecsService.service.connections.allowFromAnyIpv4(EPHEMERAL_PORT_RANGE);
-
-    new cdk.CfnOutput(this, "networkLoadBalancerURL", {
-      value: "https://"+ecsService.loadBalancer.loadBalancerDnsName,
-      description: "Network LoadBalancer URL"
-    });
+    service.targetGroup.configureHealthCheck({
+      path: "/health"
+    })
   }
 }
